139c545dbfbb9b7729c93f6ee84a194200e7fdb6
Robert Moore <me@robdmoore.id.au>
2012-04-27 01:50:26 +0800

    Added implementation for first custom validation approach

---
+++ UnobtrusiveMVCTechniques/Controllers/CustomValidationController.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using UnobtrusiveMVCTechniques.Repositories;
+
+namespace UnobtrusiveMVCTechniques.Controllers
+{
+    public class CustomValidationController : Controller
+    {
+        #region Setup
+        private readonly IUserRepository _userRepository;
+
+        public CustomValidationController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        #endregion
+
+        #region 1. Custom validation code in controller
+        public ActionResult CustomCodeInController(ViewModel1 vm)
+        {
+            if (!ModelState.IsValid)
+                return View("ValidationTest", vm);
+
+            if (_userRepository.GetUserByUserName(vm.UserName) != null)
+                ModelState.AddModelError("UserName", "That username is already taken; please try another username."); // In reality you would try not to use magic strings here
+
+            if (!ModelState.IsValid)
+                return View("ValidationTest", vm);
+
+            ViewBag.Success = true;
+            return View("ValidationTest");
+        }
+        public class ViewModel1 : TestViewModel { }
+        #endregion
+
+    }
+
+    public class TestViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+    }
+}
---
+++ UnobtrusiveMVCTechniques/Repositories/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace UnobtrusiveMVCTechniques.Repositories
+{
+    public class User
+    {
+        public string UserName { get; set; }
+    }
+
+    public interface IUserRepository
+    {
+        User GetUserByUserName(string userName);
+    }
+
+    public class UserRepository : IUserRepository
+    {
+        public User GetUserByUserName(string userName)
+        {
+            // In reality this would obviously check a datasource of some sort
+            if (userName == "dummy")
+            {
+                return new User {UserName = userName};
+            }
+            return null;
+        }
+    }
+}

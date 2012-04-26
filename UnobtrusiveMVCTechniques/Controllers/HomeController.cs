6f0d6099a8743aec72a9eadbae18809a28ec4aef
Robert Moore <me@robdmoore.id.au>
2012-04-26 23:48:35 +0800

    Added initial controller / view

---
+++ UnobtrusiveMVCTechniques/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace UnobtrusiveMVCTechniques.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- UnobtrusiveMVCTechniques/Global.asax.cs
+++ UnobtrusiveMVCTechniques/Global.asax.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace UnobtrusiveMVCTechniques
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class MvcApplication : System.Web.HttpApplication
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)

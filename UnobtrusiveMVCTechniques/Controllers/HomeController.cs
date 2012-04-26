dc7598b9d2164e433c090621913c845ec94cfb98
Robert Moore <me@robdmoore.id.au>
2012-04-27 01:33:23 +0800

    Added viewmodel with data type and default data annotations validations

--- UnobtrusiveMVCTechniques/Controllers/HomeController.cs
+++ UnobtrusiveMVCTechniques/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace UnobtrusiveMVCTechniques.Controllers
 {
@@ -6,7 +7,32 @@ namespace UnobtrusiveMVCTechniques.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(new SomeViewModel());
         }
+
+        [HttpPost]
+        public ActionResult Index(SomeViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            ViewBag.Success = true;
+            return View(vm);
+        }
+    }
+
+    public class SomeViewModel
+    {
+        #region Data type validation
+        public int Integer { get; set; }
+        #endregion
+
+        #region System.ComponentModel.DataAnnotations
+        [Required]
+        public string Required { get; set; }
+
+        [RegularExpression(@"\w")]
+        public string RegexOneWordChar { get; set; }
+        #endregion
     }
 }
---
+++ UnobtrusiveMVCTechniques/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace UnobtrusiveMVCTechniques.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        /// <summary>
+        /// Renders a textbox field with the corresponding label and validation messages all wrapped in a &lt;p&gt; tag.
+        /// </summary>
+        /// <typeparam name="TModel">Model type</typeparam>
+        /// <typeparam name="TAttribute">Model attribute type for field being rendered</typeparam>
+        /// <param name="htmlHelper">The Html helper for the model</param>
+        /// <param name="field">An expression supplying the field in the model to render</param>
+        /// <returns>An IHtmlString object that can be rendered to the page</returns>
+        public static IHtmlString OutputField<TModel, TAttribute>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TAttribute>> field)
+        {
+            var s = new StringBuilder();
+
+            s.Append("\r\n\t<p>");
+            s.Append(htmlHelper.LabelFor(field));
+            s.Append(" ");
+            s.Append(htmlHelper.TextBoxFor(field));
+            s.Append("<br />");
+            s.Append(htmlHelper.ValidationMessageFor(field));
+
+            return new HtmlString(s.ToString());
+        }
+    }
+}
